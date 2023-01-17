bdd56b0a3ce9dc7189ebdd81b9a4ab95c6091749
tpisel <mail@tompisel.com>
2023-01-18 00:41:05 +1100

    temperature analysis

--- scripts/scratch.R
+++ scripts/scratch.R
@@ -1,30 +1,72 @@
 
 
-data %>%
-  filter(Week < as.Date("2020-01-01 UTC")) %>%
-  select(Week,State) %>%
-  group_by(Week,State) %>% 
-  summarise(Cases = n()) %>% 
-  as_tsibble(key = State, index = Week, regular = FALSE) %>% 
-  index_by(t = floor_date(Week, unit = "1 week")) %>%
-  summarise(Cases = sum(Cases)) %>%
-  as_tibble() %>%
-  select(t,Cases) %>%
-  transmute(t = as.Date(t),Cases=Cases) %>%
-  as_tsibble(index = t) %>%
-  model(feasts::STL(Cases)) %>% 
-  components() %>%
+
+fit_model <- combined_data %>% 
+  model(
+    lm = TSLM(cases ~ avg_temp)
+  )
+
+
+fc_model <- forecast(fit_model)
+
+fc_model %>%  autoplot(cases)
+
+
+
+combined_data %>%
+  filter(state == "NSW") %>% 
+  autoplot(avg_temp)
+
+
+combined_data %>%
+  filter(state == "NSW") %>% 
+  gg_tsdisplay(difference(avg_temp), plot_type = "partial")
+
+
+
+
+
+
+ts <- combined_data %>% fill_gaps
+
+
+
+ts %>% 
+    model(arima = ARIMA(log(cases) ~ 0 + pdq(0, 1, 1) + PDQ(0, 1, 1))) %>%
+      report()
+
+
+ts %>% 
+  filter(state == "Vic") %>% 
+  pivot_longer(c(avg_temp,cases), names_to = "Series") %>%
   autoplot()
 
+ts %>% 
+  filter(state == "Vic") %>% 
+  model(TSLM(cases ~ avg_temp)) %>% 
+  report()
 
-data %>%
-  filter(Week < as.Date("2020-01-01 UTC")) %>%
-  group_by(Week) %>% 
-  summarise(Cases = n()) %>%
-  transmute(t = as.Date(floor_date(Week, unit = "1 week")), Cases) %>% 
-  as_tsibble(index = t) %>%
-  model(feasts::STL(Cases)) %>% 
-  components() %>%
-  autoplot()
 
-# model(stl = STL(Cases))
+
+?auto.arima
+
+
+library(modelr)
+
+
+
+mod <- lm(log(cases) ~ factor(date), data = ts)
+
+
+ts %>%  add_predictions(mod) %>% 
+  ggplot(aes(date,pred))+geom_line()
+
+
+ts %>%  add_residuals(mod) %>% 
+  ggplot(aes(date,resid))+geom_line()
+
+
+
+
+
+
